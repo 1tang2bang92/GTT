12c54534803d57db8fc8184216008d86d174d5a3
구잘 <dksgusals99@gmail.com>
2022-01-17 11:22:33 +0900

    Update

--- backend/src/plugins/cors.ts
+++
@@ -1,6 +0,0 @@
-import fp from 'fastify-plugin'
-import cors, { FastifyCorsOptions } from 'fastify-cors'
-
-export default fp<FastifyCorsOptions>(async (fastify, opts) => {
-  await fastify.register(cors)
-})
--- backend/src/plugins/websocket.ts
+++
@@ -1,6 +0,0 @@
-import fp from 'fastify-plugin'
-import websocket, { WebsocketPluginOptions } from 'fastify-websocket'
-
-export default fp<WebsocketPluginOptions>(async (fastify, opts) => {
-  await fastify.register(websocket)
-})
